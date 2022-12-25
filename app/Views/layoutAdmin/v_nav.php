eeab0a284062cafd9343a8b4776eff3cd6fa9a39
renaldin <renaldinoviandi0@gmail.com>
2022-12-25 12:51:01 +0700

    update navbar

--- app/Views/layoutAdmin/v_nav.php
+++ app/Views/layoutAdmin/v_nav.php
@@ -18,42 +18,27 @@
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="far fa-file-alt"></i>
-              <span> Reports <i class="fas fa-angle-down"></i> </span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Daily Report</a>
-              <a class="dropdown-item" href="#">Weekly Report</a>
-              <a class="dropdown-item" href="#">Yearly Report</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="products.html">
-              <i class="fas fa-shopping-cart"></i> Products
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="accounts.html">
-              <i class="far fa-user"></i> Accounts
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="fas fa-cog"></i>
-              <span> Settings <i class="fas fa-angle-down"></i> </span>
+              <span> Lomba <i class="fas fa-angle-down"></i> </span>
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Profile</a>
-              <a class="dropdown-item" href="#">Billing</a>
-              <a class="dropdown-item" href="#">Customize</a>
+              <a class="dropdown-item" href="#">Desain Maskot</a>
+              <a class="dropdown-item" href="#">Desain Web</a>
+              <a class="dropdown-item" href="#">Desain Poster</a>
+              <a class="dropdown-item" href="#">Photography</a>
+              <a class="dropdown-item" href="#">Short Movie</a>
             </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">
+              <i class="far fa-user"></i> Pengguna
+            </a>
           </li>
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link d-block" href="login.html">
-              Admin, <b>Logout</b>
+            <a class="nav-link d-block" href="<?= base_url('auth/logout') ?>">
+              <?= session()->get('nama') ?>, <b>Logout</b>
             </a>
           </li>
         </ul>
