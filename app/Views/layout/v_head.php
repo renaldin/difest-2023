a214ef3c05dc2db2f47a9c1c28222ce543a461a6
renaldin <renaldinoviandi0@gmail.com>
2022-12-26 21:50:44 +0700

    update favicon

--- app/Views/layout/v_head.php
+++ app/Views/layout/v_head.php
@@ -5,16 +5,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
   <title>Difest | Himmi Polsub</title>
-
-  <!-- Bootstrap core CSS -->
+  <link rel="icon" type="image/png" href="<?= base_url('gambar/logo.png') ?>">
   <link href="<?= base_url() ?>/../public/source/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-
-  <!-- Additional CSS Files -->
   <link rel="stylesheet" href="<?= base_url() ?>/../public/assets/css/fontawesome.css">
   <link rel="stylesheet" href="<?= base_url() ?>/../public/assets/css/style.css">
   <link rel="stylesheet" href="<?= base_url() ?>/../public/assets/css/owl.css">
--- app/Views/layoutAdmin/v_head.php
+++ app/Views/layoutAdmin/v_head.php
@@ -6,17 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Difest2023 - <?= $title ?></title>
+  <link rel="icon" type="image/png" href="<?= base_url('gambar/logo.png') ?>">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700" />
-  <!-- https://fonts.google.com/specimen/Roboto -->
   <link rel="stylesheet" href="<?= base_url('templateAdmin') ?>/css/fontawesome.min.css" />
-  <!-- https://fontawesome.com/ -->
   <link rel="stylesheet" href="<?= base_url('templateAdmin') ?>/css/bootstrap.min.css" />
-  <!-- https://getbootstrap.com/ -->
   <link rel="stylesheet" href="<?= base_url('templateAdmin') ?>/css/templatemo-style.css">
-  <!--
-	Product Admin CSS Template
-	https://templatemo.com/tm-524-product-admin
-	-->
 </head>
 
 <body id="reportsPage">
