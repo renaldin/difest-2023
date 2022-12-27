a826b867d05333065e64f342a11d0b292cc01a7a
renaldin <renaldinoviandi0@gmail.com>
2022-12-27 09:56:17 +0700

    update form validation

--- app/Views/layout/v_footer.php
+++ app/Views/layout/v_footer.php
@@ -1,4 +1,3 @@
-
 <!-- /.content -->
 <footer>
     <div class="container">
@@ -28,10 +27,7 @@
                 </div>
             </div>
             <div class="col-lg-4 col-sm-12">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.6493193695346!2d107.8256587142722!3d-6.565870366010456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e693b9ae39cc0eb%3A0x76db7b3959df2011!2sPoliteknik%20Negeri%20Subang%2C%20Kampus%20II!5e0!3m2!1sid!2sid!4v1669213764820!5m2!1sid!2sid"
-                    width="100%" height="250" style="border:0; border-radius:20px;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.6493193695346!2d107.8256587142722!3d-6.565870366010456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e693b9ae39cc0eb%3A0x76db7b3959df2011!2sPoliteknik%20Negeri%20Subang%2C%20Kampus%20II!5e0!3m2!1sid!2sid!4v1669213764820!5m2!1sid!2sid" width="100%" height="250" style="border:0; border-radius:20px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
@@ -46,6 +42,14 @@
 <script src="<?= base_url() ?>/../public/assets/js/popup.js"></script>
 <script src="<?= base_url() ?>/../public/assets/js/custom.js"></script>
 <script src="<?= base_url() ?>/../public/assets/js/timer.js"></script>
+
+<script>
+    window.setTimeout(function() {
+        $(".alert").fadeTo(1500, 0).slideUp(1500, function() {
+            $(this).remove();
+        });
+    }, 8000);
+</script>
 
 </body>
 
--- app/Views/pendaftaran/maskot/inputDataMaskot.php
+++ app/Views/pendaftaran/maskot/inputDataMaskot.php
@@ -20,7 +20,19 @@
 </div>
 
 <div class="page-content">
-    <?php $errors = session()->getFlashdata('errors'); ?>
+    <?php
+    $errors = session()->getFlashdata('errors');
+    if (!empty($errors)) { ?>
+        <div class="alert bg-danger" role="alert" style="padding: 10px 30px 10px 30px; ">
+            <ul>
+                <?php foreach ($errors as $key => $value) { ?>
+                    <li>
+                        <?= esc($value); ?>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+    <?php } ?>
     <form action="<?= base_url('pendaftaran/insertDataMaskot') ?>" method="POST" enctype="multipart/form-data">
         <div class="row">
             <div class="col-lg-6 COL-SM-12">
@@ -32,30 +44,25 @@
                 <div class="form-daftar">
                     <label>Email <small class="text-danger">*</small><small class="text-danger"> (exp: example@gmail.com)</small></label>
                     <input type="email" class="form-control" name="email" placeholder="Masukkan Email Aktif Anda" value="<?= old('email') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['email'] ?></small><?php } ?>
                 </div>
 
                 <div class="form-daftar">
                     <label>Nama Lengkap <small class="text-danger">*</small></label>
                     <input type="text" class="form-control" name="nama_peserta" placeholder="Masukkan Nama Lengkap" value="<?= old('nama_peserta') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['nama_peserta'] ?></small><?php } ?>
                 </div>
 
                 <div class="form-daftar">
                     <label>Alamat <small class="text-danger">*</small></label>
                     <input type="text" class="form-control" name="alamat" placeholder="Masukkan Alamat" value="<?= old('alamat') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['alamat'] ?></small><?php } ?>
                 </div>
                 <div class="form-daftar">
                     <label>Asal Instansi <small class="text-danger">*</small></label>
                     <input type="text" class="form-control" name="instansi" placeholder="Masukkan Asal Instansi" value="<?= old('instansi') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['instansi'] ?></small><?php } ?>
                 </div>
 
                 <div class="form-daftar">
                     <label>Nomor Whatsapp <small class="text-danger">*</small><small class="text-danger"> (exp: 0838345405878)</small></label>
                     <input type="text" class="form-control" name="wa" placeholder="Masukkan Nomor Whatsapp" value="<?= old('wa') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['wa'] ?></small><?php } ?>
                 </div>
 
             </div>
@@ -63,27 +70,22 @@
                 <div class="form-daftar">
                     <label>Scan Kartu Pelajar <small class="text-danger">*</small></label>
                     <input type="file" class="form-control pt-3" name="scan_kartu" value="<?= old('scan_kartu') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['scan_kartu'] ?></small><?php } ?>
                 </div>
                 <div class="form-daftar">
                     <label>Bukti Follow Instagram <a href="<?= base_url('https://www.instagram.com/difest2023/') ?>">@difest2023</a> <small class="text-danger">*</small></label>
                     <input type="file" class="form-control pt-3" name="bukti_igdifest" value="<?= old('bukti_igdifest') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['bukti_igdifest'] ?></small><?php } ?>
                 </div>
                 <div class="form-daftar">
                     <label>Bukti Follow Instagram <a href="<?= base_url('https://www.instagram.com/himmi.polsub/') ?>">@himmi.polsub</a> <small class="text-danger">*</small></label>
                     <input type="file" class="form-control pt-3" name="bukti_ighimmi" value="<?= old('bukti_ighimmi') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['bukti_ighimmi'] ?></small><?php } ?>
                 </div>
                 <div class="form-daftar">
                     <label>Bukti Subscribe Youtube <a href="<?= base_url('https://www.youtube.com/@mipolsubofficial7516/') ?>">MI Polsub Official</a> <small class="text-danger">*</small></label>
                     <input type="file" class="form-control pt-3" name="bukti_ythimmi" value="<?= old('bukti_ythimmi') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['bukti_ythimmi'] ?></small><?php } ?>
                 </div>
                 <div class="form-daftar">
                     <label>Bukti Pembayaran <small class="text-danger">*</small></label>
                     <input type="file" class="form-control pt-3" name="bukti_pembayaran" value="<?= old('bukti_pembayaran') ?>">
-                    <?php if (!empty($errors)) { ?><small class="text-danger"><?= $errors['bukti_pembayaran'] ?></small><?php } ?>
                 </div>
 
             </div>
